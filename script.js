2c2dbbc939485894d523b306115773962ad77038
Nnamdi <nnamdidanielosuji@gmail.com>
2021-12-28 05:20:53 +0100

    UI revamp

---
+++ script.js
@@ -0,0 +1,6 @@
+const timeEl = document.getElementById('time')
+const AM_PM_EL = document.getElementById('am-pm')
+const date= document.getElementById('date')
+const timeZoneEl = document.getElementById('timeZone')
+const cordinatesEl = document.getElementById('cordinates')
+const currentDay = document.getElementById('monday')
